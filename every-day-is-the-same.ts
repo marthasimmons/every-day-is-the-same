96e28c8256124ed607a47e106423eebcc7e04e0c
marthasimmons <marthasimmons2002@hotmail.com>
2021-05-17 11:15:05 +0100

    every day is the same

---
+++ every-day-is-the-same.ts
@@ -0,0 +1,25 @@
+const whatAmIDoingOn = (weekday: string) => {
+    switch (weekday) {
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+        case "Saturday":
+        case "Sunday":
+            console.log("I am staring at the wall, on a " + weekday)
+            return
+        default:
+            console.log("I am staring at the wall, on a non standard weekday")
+            return
+    }
+}
+
+whatAmIDoingOn("Monday")
+whatAmIDoingOn("Tuesday")
+whatAmIDoingOn("Wednesday")
+whatAmIDoingOn("Thursday")
+whatAmIDoingOn("Friday")
+whatAmIDoingOn("Saturday")
+whatAmIDoingOn("Sunday")
+whatAmIDoingOn("Christmas")
